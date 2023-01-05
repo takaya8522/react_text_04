a5604543f169ab5cbc3a1afdff00aeb205e78b80
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-05 22:51:00 +0900

    🔧Fragment追加

--- src/075_fragment/start/components/Child.js
+++ src/075_fragment/start/components/Child.js
@@ -1,10 +1,20 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim dolorum
+        commodi quibusdam consequuntur cum maiores ut suscipit ratione accusamus
+        quia reiciendis reprehenderit, maxime debitis quidem illum ducimus.
+        Neque, commodi delectus.
+      </p>
+    </React.Fragment>
   );
 };
 
