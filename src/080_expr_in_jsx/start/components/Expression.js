d9e99caaaf6f89a3b909dcf3b06721a8c8cca20e
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-05 23:25:26 +0900

    🔧JSX内でJSコード記述

---
+++ src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,11 @@
+import "./Expression.css";
+
+const Expression = () => {
+  const title = "Expression";
+  return  (
+    <div className={title.toLowerCase()}>
+      <h3>Hello {title}</h3>
+    </div>
+  )
+}
+export default Expression;
