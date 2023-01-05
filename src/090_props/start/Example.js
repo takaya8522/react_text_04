56b4101921b9c83d97e5794feb6329fb63b03284
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-06 01:00:31 +0900

    🔧props追加

--- src/090_props/start/Example.js
+++ src/090_props/start/Example.js
@@ -1,5 +1,13 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => {
+  return(
+    <>
+       <Child color=""/>
+       <Child color="green"/>
+       <Child color="red"/>
+    </>
+  )
+};
 
 export default Example;
